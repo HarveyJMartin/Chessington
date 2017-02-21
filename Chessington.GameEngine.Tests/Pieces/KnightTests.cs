3f0e833814a1939a240190605037bd31331f2b6d
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:51:35 +0000

    Added Unit Tests for Knight movement

--- Chessington.GameEngine.Tests/Pieces/KnightTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KnightTests.cs
@@ -1,6 +1,11 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using Chessington.GameEngine.Pieces;
+=======
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+>>>>>>> 9fdf317... Added Unit Tests for Knight movement
 =======
 ﻿using System.Collections.Generic;
 using Chessington.GameEngine.Pieces;
@@ -18,6 +23,7 @@ namespace Chessington.GameEngine.Tests.Pieces
     public class KnightTests
     {
         [Test]
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         public void WhiteKnights_LongUpRight()
@@ -66,6 +72,8 @@ namespace Chessington.GameEngine.Tests.Pieces
 =======
 =======
 >>>>>>> 9fdf317... Added Unit Tests for Knight movement
+=======
+>>>>>>> 9fdf317... Added Unit Tests for Knight movement
         public void Knights_CanPerformKnightsMoves()
         {
             var board = new Board();
@@ -105,6 +113,9 @@ namespace Chessington.GameEngine.Tests.Pieces
 
             moves.Should().Contain(Square.At(2, 5));
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 9fdf317... Added Unit Tests for Knight movement
+=======
 >>>>>>> 9fdf317... Added Unit Tests for Knight movement
 =======
 >>>>>>> 9fdf317... Added Unit Tests for Knight movement
