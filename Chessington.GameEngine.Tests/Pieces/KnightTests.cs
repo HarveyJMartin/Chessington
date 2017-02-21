f76afe2791c31ae67fdc8238dd7287fe4529ef19
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:51:35 +0000

    cherry picking fix

--- Chessington.GameEngine.Tests/Pieces/KnightTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KnightTests.cs
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 ﻿using Chessington.GameEngine.Pieces;
+=======
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+>>>>>>> 9fdf317... Added Unit Tests for Knight movement
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -8,6 +13,7 @@ namespace Chessington.GameEngine.Tests.Pieces
     public class KnightTests
     {
         [Test]
+<<<<<<< HEAD
         public void WhiteKnights_LongUpRight()
         {
             var board = new Board();
@@ -51,6 +57,46 @@ namespace Chessington.GameEngine.Tests.Pieces
             var moves = knight.GetAvailableMoves(board);
 
             moves.Should().Contain(Square.At(4, 7));
+=======
+        public void Knights_CanPerformKnightsMoves()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            board.AddPiece(Square.At(4, 4), knight);
+
+            var moves = knight.GetAvailableMoves(board);
+
+            var expectedMoves = new List<Square>
+            {
+                Square.At(2, 5),
+                Square.At(2, 3),
+                Square.At(3, 6),
+                Square.At(3, 2),
+                Square.At(5, 6),
+                Square.At(5, 2),
+                Square.At(6, 5),
+                Square.At(6, 3)
+            };
+
+            moves.ShouldAllBeEquivalentTo(expectedMoves);
+        }
+
+        [Test]
+        public void Knights_CanJumpOverPieces()
+        {
+            var board = new Board();
+            var knight = new Knight(Player.White);
+            board.AddPiece(Square.At(4, 4), knight);
+
+            var firstPawn = new Pawn(Player.White);
+            var secondPawn = new Pawn(Player.Black);
+            board.AddPiece(Square.At(3, 4), firstPawn);
+            board.AddPiece(Square.At(3, 5), secondPawn);
+
+            var moves = knight.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(2, 5));
+>>>>>>> 9fdf317... Added Unit Tests for Knight movement
         }
     }
 }
